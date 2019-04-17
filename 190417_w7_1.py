822d6b644fe916e331389ed6b0c0a9e9a18125b6
Lim Jongchan <nowwhd987@naver.com>
2019-04-18 02:28:29 +0900

    seventh commit

--- 190417_w7_1.py
+++ 190417_w7_1.py
@@ -16,7 +16,12 @@ def match(a,b):
         if(lista[n] == int(listb[n])):
             strike += 1
     return strike
-
+def finish():
+    inp = input()
+    if (inp == "yes"):
+        main()
+    if (inp != "no"):
+        finish()
 
 def game(a):
     print("====================\n\nGuess Numbers(000 ~999):")
@@ -25,14 +30,15 @@ def game(a):
     print(num , "strike, " , 3-num, " ball.")
     if(num == 3):
         print('Yes! The secret number is "', a,'"! You have Won!\nDo you want to play again?(yes or no)')
-        if(input() == "yes"):
-            main()
+        finish()
+
     else:
         game(a)
 
 def main():
     print("S T A R T")
     a = random.randint(12, 988)
+    print ("**Answer : ",a)
     game(a)
 
 main()
